15221e438783eb7466014f19c000ec514443dc22
Владимир Матвеев <110328797+VladimirMatveev98@users.noreply.github.com>
2022-08-30 10:46:06 +0300

    Add files via upload

---
+++ функция fibonacci v1.py
@@ -0,0 +1,18 @@
+n_not_change = True
+
+def fib(n: int) -> int:
+    """Возвращает n-нное число фибоначи"""
+    global n_not_change
+    if n_not_change:
+        n = n - 1
+        n_not_change = False
+
+    if n < 2: #Базовый случай
+        return n
+    #Рекурсивный случай:
+    return fib(n - 1) + fib(n - 2)
+
+
+if __name__ == '__main__':
+    print ("expectation 144")
+    print (fib(13))

1cf184f87b1cb8519612551c372d4429e7a89686
Владимир Матвеев <110328797+VladimirMatveev98@users.noreply.github.com>
2022-09-03 10:53:22 +0300

    Update функция fibonacci v2.py
    Проведён небольшой рефакторинг кода. Исправлена ошибка, из-за которой возвращалось не n-нное число последовательности, а n+1. Удалено лишнее.

--- функция fibonacci v2.py
+++ функция fibonacci v2.py
@@ -1,21 +1,19 @@
 memo = {0:0, 1:1} #базовые случаи
-n_not_change = True
 
-def fib2(n: int) -> int:
-    global n_not_change
-    if n_not_change:
-        n = n - 1
-        n_not_change = False
+def fib2(a):
+    #Уменьшает n для корректного результата
+    n = int(a) - 1
+    return calc2(n)
 
+def calc2(n):
+    #Вычисление с применением мемоизации:
     if n not in memo:
-        memo[n] = fib2(n - 1) + fib2(n - 2) #мемоизация
+        memo[n] = calc2(n - 1) + calc2(n - 2) #мемоизация
     return memo[n]
-
 
 
 if __name__ == '__main__':
     print ("expectation 3")
     print(fib2(5))
-    n_not_change = True
     print ("expectation 7778742049")
     print(fib2(50))

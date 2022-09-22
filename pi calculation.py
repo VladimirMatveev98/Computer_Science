5acce574e2379055d37b2dd0d7e2c265d69fc32d
Владимир Матвеев <110328797+VladimirMatveev98@users.noreply.github.com>
2022-09-22 14:02:07 +0300

    Update pi calculation.py

--- pi calculation.py
+++ pi calculation.py
@@ -3,14 +3,17 @@ def calculate_pi(n_terms):
     с 15-ю символами после запятой. Тем не менее, чем выше показатель
     n_terms, тем точнее будет это число из-за округления не отображаемых
     значений, но и время на вычисления будет увеличиваться."""
+    
     numerator = float(4)
     denominator = float(1)
     operation = float(1)
     pi = float(0)
+    
     for i in range(n_terms):
         pi += operation * (numerator / denominator)
         denominator += 2
         operation *= -1
+        
     return pi
 
 

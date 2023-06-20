828d953e8c655486861d01c397242f1b99f2622f
Владимир Матвеев <110328797+VladimirMatveev98@users.noreply.github.com>
2023-06-20 19:08:03 +0300

    Add files via upload

---
+++ class Matrix_v2.py
@@ -0,0 +1,23 @@
+class Matrix:
+    def __init__(self):
+        self.mat = []
+
+    def __str__(self):
+        #Реализовать вывод на печать
+        return "in progress"
+
+
+
+
+if __name__ == '__main__':
+
+    test_1 = [[1, 2, 3],
+              [2, 3, 4],
+              [3, 4, 5]]
+
+    test_2 = [[9, 8, 7],
+              [8, 7, 6],
+              [7, 6, 5]]
+
+    m1 = Matrix()
+    m2 = Matrix()

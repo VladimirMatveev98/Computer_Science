9f67c226983de91e6766463c86bf3af95ee7be74
Владимир Матвеев <110328797+VladimirMatveev98@users.noreply.github.com>
2023-12-02 21:33:37 +0300

    Add files via upload

---
+++ multithreading_factorial.py
@@ -0,0 +1,91 @@
+import threading
+
+import time
+
+TEST_CONST = 250000
+
+def check_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        print('[*] Время выполнения: {} секунд.'.format(end-start))
+    return wrapper
+
+
+@check_time
+def factorial(number:int) -> int:
+    res = 1
+    number = int(number)
+    for i in range(2,number+1):
+        res = res * i
+
+        #if i % 500 == 0:
+            #print("Активные потоки: ", threading.active_count())
+            #print("thr-1 is alive: ", thr_1.is_alive())
+            #print("Итерация: ", i)
+            #print("-" * 22)
+
+    print("Обычная функция завершила работу!")
+    return res
+
+
+@check_time
+def mult_factorial(number:int) -> int:
+    def part_fact(start, end):
+        """Возвращает произведение всех чисел от start
+        до end включительно"""
+        res = 1
+        for i in range(start+1, end+1):
+            res = res * i
+
+            #if i % 5000 == 0:
+                #print("Многопоточные вычисления.")
+                #print("Активные потоки: ", threading.active_count())
+                #print("Итерация: ", i)
+                #print("-" * 22)
+
+        return res
+
+    def main(number):
+        """Возвращает факториал числа, вычисляя его по частям"""
+
+        number = int(number)
+
+        #Аргументы для потоков
+        arg_2 = (1, number//4)
+        arg_3 = ((number//4)-1, (number//4)*2)
+        arg_4 = (((number//4)*2)-1, (number//4)*3)
+        arg_5 = (((number//4)*3)-1, number)
+
+        #Описание потоков
+        threads = [
+        threading.Thread(target=part_fact, args=(arg_2),name='thr-2'),
+        threading.Thread(target=part_fact, args=(arg_3),name='thr-3'),
+        threading.Thread(target=part_fact, args=(arg_4),name='thr-4'),
+        threading.Thread(target=part_fact, args=(arg_5),name='thr-5')
+        ]
+
+        #Запуск потоков
+        for thread in threads:
+            thread.start()
+
+        #Ожидание завершения потоков
+        for thread in threads:
+            thread.join()
+
+
+        #Вычисление результата, возврат результата
+        #?????
+    main(number)
+    print("Многопоточная функция завршила работу!")
+
+
+
+if __name__ == '__main__':
+    thr_1 = threading.Thread(target=factorial,args=(str(TEST_CONST),),name='thr-1')
+    thr_1.start()
+    #thr_1.join()
+
+    thr = threading.Thread(target=mult_factorial,args=(str(TEST_CONST),),name='thr-10')
+    thr.start()
